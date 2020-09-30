129df59d8636944ede106179fe1984831e61b8f4
Sakuragi97 <yasser.elbachiri@gmail.com>
2020-09-30 19:53:53 +0100

    Day 14: Scope

---
+++ 30 Days of Code/Day14_Scope.py
@@ -0,0 +1,24 @@
+from math import *
+
+class Difference:
+    def __init__(self, a=[]):
+        self.__elements = a
+        self.maximumDifference = 0
+
+	# Add your code here
+    def computeDifference(self):
+        for i in range(len(self.__elements)):
+            for j in range(len(self.__elements)-1):
+                diff = abs(self.__elements[i] - self.__elements[j+1])
+                if diff > self.maximumDifference:
+                    self.maximumDifference = diff
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
